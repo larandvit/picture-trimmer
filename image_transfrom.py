dfd0b84d73d38219f9a489573969441478300958
larandvit <33189579+larandvit@users.noreply.github.com>
2018-12-15 19:47:13 -0500

    Separated logic into CLI and GUI

--- image_transfrom.py
+++ image_transfrom.py
@@ -1,4 +1,5 @@
 import os
+from os import path
 import sys
 import argparse
 from argparse import RawTextHelpFormatter
@@ -6,67 +7,113 @@ import traceback
 
 from PIL import Image
 
-fileExtensions = ('.jpg', '.bmp', '.png')
+from enum import IntEnum
 
-returnCode = 1
+class ProcessingMode(IntEnum):
+    Cli = 1
+    Gui = 2
 
-try:
-    
-    appArgs = argparse.ArgumentParser(description='Reduce size of pictures for publishing them to web', 
-                                      epilog="Exit codes: 0 - successful completion, 1 - completion with any error",
-                                      formatter_class=RawTextHelpFormatter)
-    
-    appArgs.add_argument("--sourcefolder", nargs=1, required=True, help="Input folder with pictures", metavar='"source folder"')
-    appArgs.add_argument("--desiredsize", nargs=1, required=True, help="Output image size", metavar='"desired size"')
-    
-    args = appArgs.parse_args()
-    
-    sourceFolder = args.sourcefolder[0]
-    desiredSize = int(args.desiredsize[0])
-    
-    originalFiles = os.listdir(sourceFolder)
-    originalFilesCount = 0 
-    for sourceFile in originalFiles:
-        if sourceFile.lower().endswith(fileExtensions):
-            originalFilesCount +=1
-            
-    fileIndex = 1
-    
-    for sourceFile in originalFiles:
-        if sourceFile.lower().endswith(fileExtensions):
-            sourceFilePath = os.path.join(sourceFolder, sourceFile)
-            sourceBareFileName, sourceFileExtension = os.path.splitext(sourceFile)
-            outputFileFolder = outputFilePath = os.path.join(sourceFolder, 'Ready')
-            outputFilePath = os.path.join(outputFileFolder, sourceBareFileName + '.jpg')
-            
-            if not os.path.exists(outputFileFolder):
-                os.mkdir(outputFileFolder)
-            
-            im = Image.open(sourceFilePath)
-     
-            old_size = im.size
-            ratio = float(desiredSize)/max(old_size)
-            new_size = tuple([int(x*ratio) for x in old_size])
-            im = im.resize(new_size, Image.ANTIALIAS)
-            new_im = Image.new("RGB", (desiredSize, desiredSize), (255,255,255))
-            new_im.paste(im, ((desiredSize - new_size[0])//2, (desiredSize  -new_size[1])//2))
-            new_im.save(outputFilePath)
-            
-            outputFileSize = os.path.getsize(outputFilePath)/1024
-            print('Processed {} out of {}: {}. Size: {:.2f} KB.'.format(fileIndex, originalFilesCount, sourceFile, outputFileSize))
-            
-            fileIndex +=1
-    
-    print()
-    print('Processing has completed successfully.')
-    
-    returnCode = 0
-    
-except IOError:
-    print("Output file can't be created for ".format(sourceFile))
-    
-except Exception as err:
-    print(traceback.format_exc())
+FILE_EXTENSIONS = ('.jpg', '.bmp', '.png', '.gif')
 
-finally:    
-    sys.exit(returnCode)
+class CliProcessing:
+    def __init__(self):
+        appArgs = argparse.ArgumentParser(description='Reduce size of pictures for publishing them to web', 
+                                          epilog="Exit codes: 0 - successful completion, 1 - completion with any error",
+                                          formatter_class=RawTextHelpFormatter)
+        
+        appArgs.add_argument("--sourcefolder", nargs=1, required=True, help="Input folder with pictures", metavar='"source folder"')
+        appArgs.add_argument("--desiredsize", nargs=1, required=True, help="Output image size", metavar='"desired size"')
+        
+        args = appArgs.parse_args()
+        
+        self.sourceFolder = args.sourcefolder[0]
+        self.desiredSize = int(args.desiredsize[0])
+        
+        self.errorDesc = ''
+        
+    def run(self):
+        
+        returnCode = 1
+        
+        try:
+        
+            originalFiles = os.listdir(self.sourceFolder)
+            originalFilesCount = 0 
+            for sourceFile in originalFiles:
+                if sourceFile.lower().endswith(FILE_EXTENSIONS):
+                    originalFilesCount +=1
+                    
+            fileIndex = 1
+            
+            for sourceFile in originalFiles:
+                if sourceFile.lower().endswith(FILE_EXTENSIONS):
+                    sourceFilePath = path.join(self.sourceFolder, sourceFile)
+                    sourceBareFileName = path.splitext(sourceFile)[0]
+                    outputFileFolder = path.join(self.sourceFolder, 'Ready')
+                    outputFilePath = path.join(outputFileFolder, sourceBareFileName + '.jpg')
+                    
+                    if not path.exists(outputFileFolder):
+                        os.mkdir(outputFileFolder)
+                    
+                    im = Image.open(sourceFilePath)
+             
+                    old_size = im.size
+                    ratio = float(self.desiredSize)/max(old_size)
+                    new_size = tuple([int(x*ratio) for x in old_size])
+                    im = im.resize(new_size, Image.ANTIALIAS)
+                    new_im = Image.new("RGB", (self.desiredSize, self.desiredSize), (255,255,255))
+                    new_im.paste(im, ((self.desiredSize - new_size[0])//2, (self.desiredSize  -new_size[1])//2))
+                    
+                    if path.exists(outputFilePath):
+                        os.remove(outputFilePath)
+                    
+                    new_im.save(outputFilePath)
+                    
+                    outputFileSize = path.getsize(outputFilePath)/1024
+                    print('Processed {} out of {}: {}. Size: {:.2f} KB.'.format(fileIndex, originalFilesCount, sourceFile, outputFileSize))
+                    
+                    fileIndex +=1
+            
+            print()
+            print('Processing has completed successfully.')
+            
+            returnCode = 0
+        
+        except IOError:
+            print("Output file can't be created for ".format(sourceFile))
+            
+        except:
+            print(traceback.format_exc())
+            
+        return returnCode
+
+class GuiProcessing:
+    def __init__(self):
+        pass
+    
+    def run(self):
+        print('GUI functionality is still in progress')
+
+if __name__=="__main__":
+    
+    returnCode = 1
+    
+    try:
+        
+        processingMode = ProcessingMode.Gui
+        
+        if len(sys.argv)>1:
+            # if any parameters, command line mode
+            processing = CliProcessing()
+            processingMode = ProcessingMode.Cli
+        else:
+            # if no parameters, show GUI
+            processing = GuiProcessing()
+        
+        returnCode = processing.run()
+        
+    except:
+        print(traceback.format_exc())
+    
+    finally:    
+        sys.exit(returnCode)
